3222cd8d19189b963188b87b03bd1a9de4cc4ce1
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2014-08-01 16:20:59 +0800

    Fixed collector

--- app/workers/collector_worker.rb
+++ app/workers/collector_worker.rb
@@ -2,23 +2,25 @@ class CollectorWorker
   include Sidekiq::Worker
   sidekiq_options queue: "high", retry: false, :backtrace => true
 
-  def perform(url, access_token_string)
+  def perform(url, access_token_string, album_id)
     photos_json = JSON.parse(open(URI.encode(url)).read)
-    CollectorWorker.perform_async("#{photos_json["paging"]["next"]}&#{access_token_string}", access_token_string) if photos_json["paging"]["next"]
-    create_photos(photos_json)
+    CollectorWorker.perform_async("#{photos_json["paging"]["next"]}&#{access_token_string}", access_token_string, album_id) if photos_json["paging"] && photos_json["paging"]["next"]
+    create_photos(photos_json, album_id)
   end
 
   private
 
-  def create_photos(hash)
+  def create_photos(hash, album_id)
     hash["data"].each do |photo|
-      create_photo(open(photo["source"]))
+      create_photo(photo, album_id)
     end
   end
 
-  def create_photo(tempfile)
-    photo = Photo.new
-    photo.photo = tempfile
+  def create_photo(hash, album_id)
+    photo = Album.find(album_id).photos.new
+    photo.photo = open(hash["source"])
+    photo.fb_id = hash["id"]
+    photo.created_at = hash["created_time"]
     photo.save!
   end
 end
--- lib/tasks/collect.rake
+++ lib/tasks/collect.rake
@@ -1,16 +1,19 @@
 namespace :collect do
   task :photos => :environment do
-    get_first_page_photos
+    ALBUMS.each do |album|
+      a = Album.create({name: album["name"], fb_id: album["fb_id"]})
+      get_photos(album["fb_id"], a.id)
+    end
   end
 end
 
-def get_first_page_photos
+def get_photos(fb_id, album_id)
   puts "Accessing Facebook..."
   url = "https://graph.facebook.com/oauth/access_token?client_id=#{FB['key']}&client_secret=#{FB['secret']}&grant_type=client_credentials"
   access_token_io = open(url)
   puts "Generating access token..."
-  a = access_token_io.gets
-  url = "https://graph.facebook.com/v2.0/257558077593647/photos?#{a}"
+  access_token = access_token_io.gets
+  url = "https://graph.facebook.com/v2.0/257558077593647/photos?#{access_token}"
 
-  CollectorWorker.perform_async(url, a)
+  CollectorWorker.perform_async(url, access_token, album_id)
 end
