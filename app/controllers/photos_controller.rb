934152f4beb365c6c1456563887b9ba3b508fadd
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2014-08-02 00:22:27 +0800

    order by time

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,9 +1,9 @@
 class PhotosController < ApplicationController
   def index
     unless params["album_id"]
-      @photos = Photo.page(params[:page])
+      @photos = Photo.order('created_at DESC').page(params[:page])
     else
-      @photos = Album.find(params[:album_id]).photos.page(params[:page])
+      @photos = Album.find(params[:album_id]).photos.order('created_at DESC').page(params[:page])
     end
   end
 end
