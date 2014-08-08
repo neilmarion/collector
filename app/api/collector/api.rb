a24b1f26a0d84a8c213618f91534eba5145964ea
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2014-08-08 16:19:28 +0800

    improved storing of read

--- app/api/collector/api.rb
+++ app/api/collector/api.rb
@@ -11,15 +11,18 @@ module Collector
           album = Album.find(params[:album_id])
           if params[:random]
             pr = PhotoRandomizer.new(params[:album_id], params[:device])
-            prev_current_next = pr.photo.prev_current_next
+            photo = pr.photo
+            prev_current_next = photo.prev_current_next
           else
-            prev_current_next = album.photos.order('created_at DESC').first.prev_current_next
+            photo = album.photos.order('created_at DESC').first
+            prev_current_next = photo.prev_current_next
           end
         else
           id  = params[:id]
-          prev_current_next = Photo.find(id).prev_current_next || Photo.find(id).album.first.prev_current_next
+          photo = Photo.find(id) || Photo.find(id).album.first
+          prev_current_next = photo.prev_current_next
         end
-        album.devices.where(mac_address: params[:device]).first_or_create.has_read(prev_current_next[:current][:id]) if params[:device] && !params[:device].blank?
+        photo.album.devices.where(mac_address: params[:device]).first_or_create.has_read(prev_current_next[:current][:id]) if params[:device] && !params[:device].blank?
         prev_current_next
       end
     end
--- app/models/device.rb
+++ app/models/device.rb
@@ -2,7 +2,12 @@ class Device < ActiveRecord::Base
   belongs_to :album
 
   def has_read(id)
-    self.read = read.nil? ? "#{id}" : "#{read},#{id}"
+    self.read = unless read.nil?
+      (read.split(',') | [id.to_s]).sort.join(',')
+    else
+      "#{id}"
+    end
+
     save!
   end
 end
