82256055ee60563e59304469d575911d955ece5b
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2014-08-27 01:28:38 +0800

    Added announcement model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140808023738) do
+ActiveRecord::Schema.define(version: 20140826172818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20140808023738) do
   create_table "albums", force: true do |t|
     t.string   "fb_id"
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "announcements", force: true do |t|
+    t.text     "message"
+    t.integer  "album_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
