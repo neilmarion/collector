ba272033e16b009a9e7657687188a5a7ea8c7a71
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2014-09-03 10:48:04 +0800

    Added views and etc for devices

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, controllers: { sessions: "admins/sessions" }
   root to: "photos#index"
   mount Sidekiq::Web, at: '/sidekiq'
   mount Collector::API => '/v1'
